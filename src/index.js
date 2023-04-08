2a372daf86d12aa77131726c5f87d12bb5c22ef5
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-04-07 22:03:05 -0600

    Add css styles

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ const tasks = [
   { description: "Clean room", completed: false, index: 3 }
 ];
 
-function renderTaskList() {
-  const taskListElement = document.getElementById("task-list");
+ function renderTaskList() {
+        const taskListElement = document.getElementById("task-list");
 
-  // Sort tasks by index value
-  tasks.sort((a, b) => a.index - b.index);
+        // Sort tasks by index value
+        tasks.sort((a, b) => a.index - b.index);
 
-  // Clear existing list items
-  taskListElement.innerHTML = "";
+        // Clear existing list items
+        taskListElement.innerHTML = "";
 
-  // Create a new list item for each task
-  tasks.forEach(task => {
-    const listItemElement = document.createElement("li");
-    listItemElement.textContent = task.description;
-    if (task.completed) {
-      listItemElement.classList.add("completed");
-    }
-    taskListElement.appendChild(listItemElement);
-  });
-}
+        // Create a new list item for each task
+        tasks.forEach(task => {
+          const listItemElement = document.createElement("li");
+          listItemElement.textContent = task.description;
+          if (task.completed) {
+            listItemElement.classList.add("completed");
+          }
+          taskListElement.appendChild(listItemElement);
+        });
+      }
 
-// Render task list on page load
-renderTaskList();
+      // Render task list on page load
+      renderTaskList();
