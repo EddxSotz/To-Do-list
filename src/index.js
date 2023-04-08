90db098ef322ac10cd7d471683f4c20fdfc5cede
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-04-07 18:40:23 -0600

    basic code structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+const tasks = [
+  { description: "Do laundry", completed: false, index: 1 },
+  { description: "Buy groceries", completed: true, index: 2 },
+  { description: "Clean room", completed: false, index: 3 }
+];
+
+function renderTaskList() {
+  const taskListElement = document.getElementById("task-list");
+
+  // Sort tasks by index value
+  tasks.sort((a, b) => a.index - b.index);
+
+  // Clear existing list items
+  taskListElement.innerHTML = "";
+
+  // Create a new list item for each task
+  tasks.forEach(task => {
+    const listItemElement = document.createElement("li");
+    listItemElement.textContent = task.description;
+    if (task.completed) {
+      listItemElement.classList.add("completed");
+    }
+    taskListElement.appendChild(listItemElement);
+  });
+}
+
+// Render task list on page load
+renderTaskList();
