a275dd53d7373a2041825b275b0012b5524ae5e0
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-05-05 00:00:53 -0600

    code structure with functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,84 @@
 import './style.css';
 
-const tasks = [
-  { description: 'Do laundry', completed: false, index: 1 },
-  { description: 'Buy groceries', completed: true, index: 2 },
-  { description: 'Clean room', completed: false, index: 3 },
-];
+const form = document.getElementById('input_form');
+const addItemInput = document.getElementById('AddToList');
 
-function renderTaskList() {
-  const taskListElement = document.getElementById('task-list');
+// Define the To-Do List array
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
-  // Sort tasks by index value
-  tasks.sort((a, b) => a.index - b.index);
+// Function to save the To-Do List in local storage
+function saveList() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
 
-  // Clear existing list items
-  taskListElement.innerHTML = '';
+// Function to add a new task to the To-Do List
+function addTask(description) {
+  // Create a new task object with default values
+  const newTask = {
+    description: description,
+    completed: false,
+    index: todoList.length
+  };
+  // Add the new task to the array
+  todoList.push(newTask);
+  // Save the updated To-Do List in local storage
+  saveList();
+}
 
-  // Create a new list item for each task
-  tasks.forEach((task) => {
-    const listItemElement = document.createElement('li');
-    const checkboxElement = document.createElement('input');
-    const listItemText = document.createElement('span');
-    checkboxElement.setAttribute('type', 'checkbox');
-    checkboxElement.setAttribute('style', 'margin-right: 1rem');
+// Function to delete a task from the To-Do List
+function deleteTask(index) {
+  // Remove the task with the given index from the array
+  todoList.splice(index, 1);
+  // Update the indexes of all remaining tasks
+  todoList.forEach((task, i) => {
+    task.index = i;
+  });
+  // Save the updated To-Do List in local storage
+  saveList();
+}
 
-    listItemText.textContent = task.description;
+// Function to edit the description of a task in the To-Do List
+function editTaskDescription(index, newDescription) {
+  // Update the description of the task with the given index
+  todoList[index].description = newDescription;
+  // Save the updated To-Do List in local storage
+  saveList();
+}
 
-    listItemElement.appendChild(checkboxElement);
-    listItemElement.appendChild(listItemText);
-
-    if (task.completed) {
-      listItemElement.classList.add('completed');
-    }
-    taskListElement.appendChild(listItemElement);
+// Function to render the To-Do List on the web page
+function renderList() {
+  const listContainer = document.getElementById('list-container');
+  // Clear the list container
+  listContainer.innerHTML = '';
+  // Iterate over the To-Do List array and create an HTML list item for each task
+  todoList.forEach((task) => {
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.addEventListener('click', () => {
+      task.completed = checkbox.checked;
+      saveList();
+    });
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteTask(task.index);
+      renderList();
+    });
+    const descriptionSpan = document.createElement('span');
+    descriptionSpan.textContent = task.description;
+    listItem.appendChild(checkbox);
+    listItem.appendChild(descriptionSpan);
+    listItem.appendChild(deleteButton);
+    listContainer.appendChild(listItem);
   });
 }
-// Render task list on page load
-renderTaskList();
+
+// Call the renderList() function to display the To-Do List on page load
+renderList();
+
+form.addEventListener('submit', () => {
+  addTask(addItemInput.value);
+  renderList();
+});
